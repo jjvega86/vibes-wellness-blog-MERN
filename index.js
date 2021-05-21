e53c1bc09d48d26927203daf26e04c03e770d0ee
JJ <jj.vega86@gmail.com>
2021-05-21 12:19:15 -0400

    Installed nodemon, added start script, updated npm

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const connectDb = require("./startup/db");
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
 connectDb();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
