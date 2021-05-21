9b0f7123c04fedb5ae84f373c685bb7e32085183
JJ <jj.vega86@gmail.com>
2021-05-21 12:00:52 -0400

    Shifted db connect logic to separate file, test successful

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const connectDb = require("./startup/db");
 
-mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDb..."))
-  .catch((err) => console.log(`Could not connect to MongoDb. Error: ${err}`));
+connectDb();
