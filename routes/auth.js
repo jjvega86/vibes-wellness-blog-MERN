bfc3319524666c55492c3a455680a0d85d3b6fe1
JJ <jj.vega86@gmail.com>
2021-05-22 16:05:35 -0400

    Added POST route to auth routes to allow a user to login using email and password

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,35 @@
 const Joi = require("joi");
+const bcrypt = require("bcrypt");
 const express = require("express");
+const { User } = require("../models/user");
 const router = express.Router();
 
-const validateLogin = (req) => {
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateLogin(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send(`Invalid email or password.`);
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password.");
+    return res.send(true);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
+function validateLogin(req) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(1024).required(),
   });
   return schema.validate(req);
-};
+}
 
 module.exports = router;
