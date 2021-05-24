17cd578877b7c022c7be5d3031209e7ec20c149d
JJ <jj.vega86@gmail.com>
2021-05-24 10:00:47 -0400

    Added initial JSX to register component

---
+++ client/src/components/register/register.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          Password:
+          <input type="text" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+};
+
+export default Register;
