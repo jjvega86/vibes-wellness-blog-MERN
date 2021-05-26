a815d0076057891ffd7cdc38830bbbdeb317770c
JJ <jj.vega86@gmail.com>
2021-05-26 14:56:38 -0400

    Added token and user state to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
-  console.log(localStorage);
+  const [currentToken, setCurrentToken] = useState(
+    localStorage.getItem("token")
+  );
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    setCurrentToken(localStorage.getItem("token"));
+    const userfromToken = jwtDecode(currentToken);
+    setCurrentUser(userfromToken);
+    console.log(currentUser);
+  }, []);
+
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
