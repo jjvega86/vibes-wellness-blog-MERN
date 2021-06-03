2b55a4b014675252e0612e4757ed7d87ad1c9b7f
JJ <jj.vega86@gmail.com>
2021-06-02 21:58:05 -0400

    Added initial logic to useCustomForm

--- client/src/hooks/useCustomForm.js
+++ client/src/hooks/useCustomForm.js
@@ -1,5 +1,22 @@
-const useCustomForm = () => {
-  console.log("My first custom hook!");
+import { useState } from "react";
+
+const useCustomForm = (callback) => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (event) => {
+    event.persist();
+    setFormData((formData) => ({
+      ...formData,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    callback();
+  };
+
+  return { formData, handleChange, handleSubmit };
 };
 
 export default useCustomForm;
