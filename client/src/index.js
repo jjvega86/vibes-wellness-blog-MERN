a31b64649cc185a384584d5e713c706d8a49acbe
JJ <jj.vega86@gmail.com>
2021-06-28 12:07:15 -0500

    Added Redux store and Provider to client

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app";
 import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
