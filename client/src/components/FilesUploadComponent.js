7e111ee690ccf3696cd7e776206461b0fc10a0a5
JJ <jj.vega86@gmail.com>
2021-08-19 19:12:19 -0400

    Added FilesUploadComponent to project

---
+++ client/src/components/FilesUploadComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const FilesUploadComponent = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <form>
+          <h3>File Upload</h3>
+          <div className="form-group">
+            <input type="file" />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Upload
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FilesUploadComponent;
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import FilesUploadComponent from "../components/FilesUploadComponent";
 
 const ProfilePage = ({ user }) => {
+  //TODO: Add image upload for profile picture
   return (
     <>
+      <FilesUploadComponent />
       <div className="col-md-3"></div>
       <div className="col-md-6">
         <h1> Hello, {user.name}!</h1>
